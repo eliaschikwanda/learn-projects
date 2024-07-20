d493b7c9757cbd30a5058d3d83db3d05e5a9be95
eliaschikwanda <chikwandaelias@gmail.com>
2024-07-20 07:59:53 +0200

    Optimized image url

--- game-hub/src/components/CriticScore.tsx
+++ game-hub/src/components/CriticScore.tsx
@@ -1,14 +1,21 @@
 import { Badge } from "@chakra-ui/react";
 
 interface CriticScoreProps {
-    score: number;
+  score: number;
 }
 
-const CriticScore = ({score} : CriticScoreProps) => {
-    let color = score > 75 ? 'green' : score > 60 ? 'yellow' : '';
+const CriticScore = ({ score }: CriticScoreProps) => {
+  let color = score > 75 ? "green" : score > 60 ? "yellow" : "";
   return (
-    <Badge  colorScheme={color} fontSize={'14px'} paddingX={2} borderRadius={'4px'}>{score}</Badge>
-  )
-}
+    <Badge
+      colorScheme={color}
+      fontSize={"14px"}
+      paddingX={2}
+      borderRadius={"4px"}
+    >
+      {score}
+    </Badge>
+  );
+};
 
-export default CriticScore
+export default CriticScore;
--- game-hub/src/components/GameCards.tsx
+++ game-hub/src/components/GameCards.tsx
@@ -1,8 +1,8 @@
-import React from "react";
 import { Game } from "../hooks/useGames";
 import { Card, Image, CardBody, Heading, HStack } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
 
 interface GameCardsProps {
   game: Game;
@@ -12,7 +12,7 @@ const GameCards = ({ game }: GameCardsProps) => {
   return (
     <>
       <Card borderRadius={10} overflow={"hidden"}>
-        <Image src={game.background_image} />
+        <Image src={getCroppedImageUrl(game.background_image)} />
         <CardBody>
           <Heading fontSize={"2xl"}>{game.name}</Heading>
           <HStack justifyContent={'space-between'}>
