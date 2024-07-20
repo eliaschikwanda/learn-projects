762ed288790c8138ebcc01a264769b5c250c049f
eliaschikwanda <chikwandaelias@gmail.com>
2024-07-20 07:45:14 +0200

    DIsplaying Critic Score

--- game-hub/src/components/GameCards.tsx
+++ game-hub/src/components/GameCards.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Game } from "../hooks/useGames";
-import { Card, Image, CardBody, Heading, Text } from "@chakra-ui/react";
+import { Card, Image, CardBody, Heading, HStack } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 
 interface GameCardsProps {
   game: Game;
@@ -14,13 +15,16 @@ const GameCards = ({ game }: GameCardsProps) => {
         <Image src={game.background_image} />
         <CardBody>
           <Heading fontSize={"2xl"}>{game.name}</Heading>
-          <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          ></PlatformIconList>
+          <HStack justifyContent={'space-between'}>
+            <PlatformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            ></PlatformIconList>
+            <CriticScore score={game.metacritic}></CriticScore>
+          </HStack>
         </CardBody>
       </Card>
     </>
   );
 };
 
-export default GameCards;
+export default GameCards
