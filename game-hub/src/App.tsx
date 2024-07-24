c6eab412db2fb624c5de3461292e7b9ef37eb58e
eliaschikwanda <chikwandaelias@gmail.com>
2024-07-24 14:17:26 +0200

    Build Platform Selector

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -4,6 +4,7 @@ import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
+import PlatformSelector from "./components/PlatformSelector";
 
 function App() {
   // The component holding the state should be the one updating it.
@@ -29,6 +30,7 @@ function App() {
           </GridItem>
         </Show>
         <GridItem area={"main"}>
+          <PlatformSelector></PlatformSelector>
           <GameGrid selectedGenre={selectedGenre}></GameGrid>
         </GridItem>
       </Grid>
