e417dc5458637ea1280d5a5e73f2e2475bec4e41
eliaschikwanda <chikwandaelias@gmail.com>
2024-07-24 14:38:45 +0200

    Filter games by platform

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -5,10 +5,12 @@ import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
+import { Platform } from "./hooks/useGames";
 
 function App() {
   // The component holding the state should be the one updating it.
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null);
 
   return (
     <>
@@ -30,8 +32,8 @@ function App() {
           </GridItem>
         </Show>
         <GridItem area={"main"}>
-          <PlatformSelector></PlatformSelector>
-          <GameGrid selectedGenre={selectedGenre}></GameGrid>
+          <PlatformSelector selectedPlatform = {selectedPlatform} onSelectPlatform={(platform) => {setSelectedPlatform(platform)}}></PlatformSelector>
+          <GameGrid selectedPlatform = {selectedPlatform} selectedGenre={selectedGenre}></GameGrid>
         </GridItem>
       </Grid>
     </>
--- game-hub/src/hooks/useGames.ts
+++ game-hub/src/hooks/useGames.ts
@@ -2,19 +2,27 @@ import useData from "./useData";
 import { Genre } from "./useGenres";
 
 export interface Platform {
-    id: number;
-    name: string;
-    slug: string;
+  id: number;
+  name: string;
+  slug: string;
 }
 
 export interface Game {
-    id: number;
-    name: string;
-    background_image: string;   
-    parent_platforms:  {platform: Platform}[];
-    metacritic: number;
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
 
-const useGames = (selectedGenres: Genre | null) => useData<Game>('/games', {params: {genre: selectedGenres?.id}}, [selectedGenres?.id]) // This is allowed by the backend for filtering
+const useGames = (
+  selectedGenres: Genre | null,
+  selectedPlatfrom: Platform | null
+) =>
+  useData<Game>(
+    "/games",
+    { params: { genre: selectedGenres?.id, platforms: selectedPlatfrom?.id } },
+    [selectedGenres?.id, selectedPlatfrom?.id]
+  ); // This is allowed by the backend for filtering
 
-export default useGames
+export default useGames;
