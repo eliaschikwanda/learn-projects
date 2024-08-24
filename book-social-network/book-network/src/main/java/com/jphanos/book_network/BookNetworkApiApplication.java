683e2aa55c0d46ff9e164b50ad36a1163d4b2966
eliaschikwanda <chikwandaelias@gmail.com>
2024-08-24 19:58:15 +0200

    Created a User class and added auditing to the field.

--- book-social-network/book-network/src/main/java/com/jphanos/book_network/BookNetworkApiApplication.java
+++ book-social-network/book-network/src/main/java/com/jphanos/book_network/BookNetworkApiApplication.java
@@ -1,9 +1,12 @@
 package com.jphanos.book_network;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // Needed for the user entity to be audited.
 public class BookNetworkApiApplication {
 
 	public static void main(String[] args) {
