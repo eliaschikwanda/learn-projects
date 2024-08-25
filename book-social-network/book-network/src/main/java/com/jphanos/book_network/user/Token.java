ad1bb9b49d0ea30b8b6de4f8afbd6b268c8594c0
eliaschikwanda <chikwandaelias@gmail.com>
2024-08-25 03:11:36 +0200

    Created the token entity and repository.

---
+++ book-social-network/book-network/src/main/java/com/jphanos/book_network/user/Token.java
@@ -0,0 +1,27 @@
+package com.jphanos.book_network.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false) // userId will be the foreign key and the field can't be null
+    private User user;
+}
