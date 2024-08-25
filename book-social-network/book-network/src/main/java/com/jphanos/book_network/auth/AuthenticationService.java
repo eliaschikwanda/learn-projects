94276ce8439d2033be8824d767e10cd363a2ab80
eliaschikwanda <chikwandaelias@gmail.com>
2024-08-25 05:05:53 +0200

    Implemented token generation algorithm

--- book-social-network/book-network/src/main/java/com/jphanos/book_network/auth/AuthenticationService.java
+++ book-social-network/book-network/src/main/java/com/jphanos/book_network/auth/AuthenticationService.java
@@ -1,11 +1,71 @@
 package com.jphanos.book_network.auth;
 
+import com.jphanos.book_network.role.RoleRepository;
+import com.jphanos.book_network.user.Token;
+import com.jphanos.book_network.user.TokenRepository;
+import com.jphanos.book_network.user.User;
+import com.jphanos.book_network.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class AuthenticationService {
 
-    public void register(RegistrationRequest request) {
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
 
+    public void register(RegistrationRequest request) {
+        var userRole = roleRepository.findByName("USER")
+                // todo - better exception handling
+                .orElseThrow(() -> new IllegalStateException("ROLE USER was not initialized"));
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .email(request.getEmail())
+                .accountLocked(false)
+                .enabled(false)
+                .roles(List.of(userRole))
+                .build();
+        userRepository.save(user);
+        sentValidationEmail(user);
+    }
+
+    private void sentValidationEmail(User user) {
+        var newToken = generateAndSaveActivationToken(user);
+        // send email
+    }
+
+    private String generateAndSaveActivationToken(User user) {
+        // generate a token
+        String generatedToken = generateActivationCode(6);
+        var token = Token.builder()
+                .token(generatedToken)
+                .createdAt(LocalDateTime.now())
+                .expiresAt(LocalDateTime.now().plusMinutes(30))
+                .user(user)
+                .build();
+        tokenRepository.save(token);
+        return generatedToken;
+    }
+
+    private String generateActivationCode(int length) {
+        String characters = "0123456789";
+        StringBuilder codeBuilder = new StringBuilder();
+        // User secure random to make sure the generated token is cryptographically secure
+        SecureRandom secureRandom = new SecureRandom();
+        for (int i = 0; i < length; i++) {
+            int randomIndex = secureRandom.nextInt(characters.length()); // 0...9
+            codeBuilder.append(characters.charAt(randomIndex));
+        }
+        return codeBuilder.toString();
     }
 }
