29a1bc8399560f2eaacf1f66de881c25427fcf8e
eliaschikwanda <chikwandaelias@gmail.com>
2024-08-26 15:47:49 +0200

    Added Swagger Api configuration

---
+++ book-social-network/book-network/src/main/java/com/jphanos/book_network/config/openApiConfig.java
@@ -0,0 +1,58 @@
+package com.jphanos.book_network.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Elias",
+                        email = "contact@jphanos.com",
+                        url = "https://jphanos.com"
+                ),
+                description = "OpenApi documention for Spring security",
+                title = "OpenApi specification - Elias",
+                version = "1.0",
+                license = @License(
+                        name = "Licence name",
+                        url = "https://licence.com"
+                ),
+                termsOfService = "Terms of Service"
+        ),
+        servers = {
+                @Server(
+                        description = "Local Environment",
+                        url = "http://localhost:8080/api/v1"
+                ),
+                @Server(
+                        description = "Prod Example",
+                        url = "https://someurl.com"
+                )
+        },
+        // For every controller class we need a security requirement
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+// Should be the same as that of the security requirement name
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        // Security for HTTP
+        type =  SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class openApiConfig {
+}
