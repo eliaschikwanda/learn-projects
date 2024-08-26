802c316de3c33a53e84cd8560e742539a81002bc
eliaschikwanda <chikwandaelias@gmail.com>
2024-08-26 05:01:34 +0200

    Added custom handling class

---
+++ book-social-network/book-network/src/main/java/com/jphanos/book_network/handler/BusinessErrorCodes.java
@@ -0,0 +1,30 @@
+package com.jphanos.book_network.handler;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
+
+public enum BusinessErrorCodes {
+    NO_CODE(0, NOT_IMPLEMENTED, "No code"),
+    INCORRECT_CURRENT_PASSWORD(300, BAD_REQUEST, "Current password is incorrect"),
+    NEW_PASSWORD_DOES_NOT_MATCH(301, BAD_REQUEST, "New Password does not match"),
+    ACCOUNT_LOCKED(302, FORBIDDEN, "User account is locked"),
+    ACCOUNT_DISABLED(303, FORBIDDEN, "User account is disabled"),
+    BAD_CREDENTIALS(304, FORBIDDEN, "Login and / or password is incorrect"),
+
+    ;
+
+    @Getter
+    private final int code;
+    @Getter
+    private final String Description;
+    @Getter
+    private final HttpStatus httpStatus;
+
+    BusinessErrorCodes(int code, HttpStatus httpStatus, String description) {
+        this.code = code;
+        Description = description;
+        this.httpStatus = httpStatus;
+    }
+}
