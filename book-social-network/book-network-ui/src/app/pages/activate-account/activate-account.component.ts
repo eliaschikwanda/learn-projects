e20ebaf6a0184d62c416113db211f0798b9cbfdd
eliaschikwanda <chikwandaelias@gmail.com>
2024-08-27 08:27:04 +0200

    Completed the account activation page

---
+++ book-social-network/book-network-ui/src/app/pages/activate-account/activate-account.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+import {AuthenticationService} from "../../services/services/authentication.service";
+import {CodeInputModule} from "angular-code-input";
+import {NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-activate-account',
+  standalone: true,
+  imports: [
+    CodeInputModule,
+    NgIf
+  ],
+  templateUrl: './activate-account.component.html',
+  styleUrl: './activate-account.component.scss'
+})
+export class ActivateAccountComponent {
+  message: string = '';
+  isOkay: boolean = true;
+  submitted: boolean = false;
+
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ) {}
+
+  onCodeCompleted(token: string) {
+    this.confirmAccount(token);
+  }
+
+  redirectToLogin() {
+    this.router.navigate(["login"]);
+  }
+
+  private confirmAccount(token: any) {
+    this.authService.confirm({
+      token
+    }).subscribe({
+      next: () => {
+        this.message = 'Your account has been succesfully activated.\n Now you can proceed to login.';
+        this.submitted = true;
+        this.isOkay = true;
+      }, error: () => {
+        this.message = 'Token has expired or invalid';
+        this.submitted = true;
+        this.isOkay = false;
+      }
+    })
+  }
+}
